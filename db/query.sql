4e48b8d889923462444c44bce37c20a5127ec870
Travis Nicholson <110001213+Travisnicholson90@users.noreply.github.com>
2023-05-09 16:28:07 +1000

    readme-complete

--- db/query.sql
+++ db/query.sql
@@ -1,10 +1,16 @@
+
+//view all departments and show department names and department ids
+SELECT * FROM department;
+
+//view all roles
+SELECT role.id, role.title, role.salary, role.department_id FROM role;
+
 -- view all employee
 SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager
 FROM employee
 LEFT JOIN role ON employee.role_id = role.id
 LEFT JOIN department ON role.department_id = department.id
 LEFT JOIN employee manager ON manager.id = employee.manager_id;
-
 
 -- add a department
 INSERT INTO department (name)
@@ -22,3 +28,6 @@ VALUES (enter first name, enter last name, enter role id, enter manager id);
 UPDATE employee
 SET role_id = enter role id
 WHERE id = enter employee id;
+
+// delete row from employee table where id = 20
+DELETE FROM employee WHERE id = 20;
